8d26bd60fd3c2f1eeeba8d87616619e1c27bbdc1
Christoph Jansen <Christoph.Jansen@htw-berlin.de>
2019-01-24 17:42:28 +0100

    clean up

--- red_connector_ssh/main.py
+++ red_connector_ssh/main.py
@@ -2,9 +2,5 @@ from cc_connector_cli.connector_cli import run_connector
 from red_connector_ssh.ssh import Ssh
 
 
-def run_ssh():
+def main():
     run_connector(Ssh)
-
-
-if __name__ == '__main__':
-    run_ssh()
