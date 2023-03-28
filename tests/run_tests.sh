ca62248d8ad906a0253023120ce5be00a2babf51
Bluemi <bruno.schilling@protonmail.ch>
2023-03-28 16:13:42 +0200

    add tests for receive-file, receive-dir, mount-dir

---
+++ tests/run_tests.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+RESULT_FILES_DIR="result-files"
+ACCESS_FILES_DIR="access-files"
+
+if [ "$(basename "$PWD")" = "red-connector-ssh" ]; then
+  cd tests || exit 1
+fi
+
+if [ -d "$RESULT_FILES_DIR" ]; then
+  rm -r "$RESULT_FILES_DIR"
+fi
+
+mkdir -p "$RESULT_FILES_DIR"
+
+# test receive-file
+jq -M -s '.[0] * .[1]' "$ACCESS_FILES_DIR/auth.json" "$ACCESS_FILES_DIR/receive-file.json" > "access.json"
+red-connector-ssh receive-file access.json "$RESULT_FILES_DIR/testfile.txt"
+
+if [ ! -f "$RESULT_FILES_DIR/testfile.txt" ]; then
+  echo "ERROR: failed to create result file with \"receive-file\""
+  exit 1
+fi
+
+# test receive-directory
+jq -M -s '.[0] * .[1]' "$ACCESS_FILES_DIR/auth.json" "$ACCESS_FILES_DIR/receive-dir.json" > "access.json"
+red-connector-ssh receive-dir access.json "$RESULT_FILES_DIR/testdir"
+
+if [ ! -d "$RESULT_FILES_DIR/testdir" ]; then
+  echo "ERROR: failed to create result directory with \"receive-dir\""
+  exit 1
+fi
+
+# test mount-directory
+jq -M -s '.[0] * .[1]' "$ACCESS_FILES_DIR/auth.json" "$ACCESS_FILES_DIR/mount-dir.json" > "access.json"
+red-connector-ssh mount-dir access.json "$RESULT_FILES_DIR/testdir_mount"
+
+if [ ! -d "$RESULT_FILES_DIR/testdir_mount" ]; then
+  echo "ERROR: failed to create result directory with \"mount-dir\""
+  exit 1
+fi
+
+red-connector-ssh umount-dir "$RESULT_FILES_DIR/testdir_mount"
